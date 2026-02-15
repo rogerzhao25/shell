111bf34fc2bdf803f84cff924a01776062405637
Roger Zhao <huaigu.zhao@gmail.com>
2026-02-14 22:07:59 -0500

    correct assignment #7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -49,7 +49,8 @@ cp ./data/raw/*user*.log  ./data/processed/user_logs/
 cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rf -rf ./data
+rm -f ./data/raw/*ipaddr*
+rm -f ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 find ./data/processed -type f | sort > ./data/inventory.txt
